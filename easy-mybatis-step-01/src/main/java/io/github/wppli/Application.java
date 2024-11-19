28ced13f245d1da3bf4bc6fa07c4842cc4260455
lijiaqiang <2148660566@qq.com>
2024-11-19 16:13:05 +0800

    easy-mybatis 框架2

--- easy-mybatis-step-01/src/main/java/io/github/wppli/Application.java
+++ easy-mybatis-step-01/src/main/java/io/github/wppli/Application.java
@@ -3,6 +3,6 @@ package io.github.wppli;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        System.out.println("aaabbb");
     }
 }
